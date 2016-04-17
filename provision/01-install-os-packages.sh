1917e17537c06e4358a4e00609e2b346cf5b4e0c
shadoxx <brandon@hive13.org>
2016-04-17 16:33:58 -0700

    Setup Elation webserver for further development

--- provision/01-install-os-packages.sh
+++ provision/01-install-os-packages.sh
@@ -17,4 +17,22 @@ apt-get install -y \
     mysql-server \
     nodejs \
     php5 \
-    php5-mysql
+    php5-mysql \
+    php5-sqlite \
+    php5-memcache
+
+## CONFIGURE APACHE
+
+# Change the user Apache runs under so we don't run into permissions issues
+sed -i -e 's/www-data/vagrant/g' /etc/apache2/envvars
+
+ln -s /vagrant/provision/submodules/elation/config/apache-elation.conf /etc/apache2/sites-available/001-elation.conf
+
+ln -s /vagrant/provision/submodules/elation /var/www/elation
+ln -s /vagrant/provision/submodules/janusweb /var/www/elation/components/janusweb
+
+a2ensite 001-elation
+a2enmod rewrite
+
+# restart apache one last time
+service apache2 restart
