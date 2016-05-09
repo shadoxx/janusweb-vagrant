19026a085f8670b8d93b07e06b0cf5d9d83d4a71
shadoxx <brandon@hive13.org>
2016-05-08 18:04:03 -0700

    Removed CORS proxy from JanusWeb configuration file

--- provision/conf/janusweb-config.js
+++ provision/conf/janusweb-config.js
@@ -3,7 +3,7 @@ elation.config.set('dependencies.host', 'localhost:8000');
 elation.config.set('dependencies.path', elation.config.get('dependencies.protocol') + '//' + elation.config.get('dependencies.host') + '/');
 elation.config.set('dependencies.main', 'janusweb.js');
 elation.config.set('janusweb.network.host', 'ws://janusweb.lnq.to:5566');
-elation.config.set('janusweb.network.corsproxy', 'http://janusweb.lnq.to:8089/');
+elation.config.set('janusweb.network.corsproxy', '');
 elation.config.set('janusweb.datapath', elation.config.get('dependencies.path') + 'media/');
 elation.config.set('janusweb.tracking.enabled', true);
 elation.config.set('janusweb.tracking.clientid', 'UA-49582649-2');
